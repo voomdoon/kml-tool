394014c361680c311e6bf48570dce19af119cbdf
voomdoon <nilreb24-github20141105@yahoo.de>
2023-01-25 13:29:41 +0100

    refactor: add main sceleton

---
+++ src/main/java/de/voomdoon/tool/map/kml/KmlToolMain.java
@@ -0,0 +1,9 @@
+package de.voomdoon.tool.map.kml;
+
+public class KmlToolMain {
+
+	public static void main(String[] args) {
+		// TODO implement main
+		throw new UnsupportedOperationException("Method 'main' not implemented yet");
+	}
+}
---
+++ src/test/java/de/voomdoon/tool/map/kml/KmlToolMainTest.java
@@ -0,0 +1,5 @@
+package de.voomdoon.tool.map.kml;
+
+class KmlToolMainTest {
+
+}
