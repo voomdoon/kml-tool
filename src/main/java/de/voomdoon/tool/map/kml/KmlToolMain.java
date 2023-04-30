8752710edde73b545fc92497455ad5a1daad0212
voomdoon <nilreb24-github20141105@yahoo.de>
2023-04-30 17:21:27 +0200

    refactor: add KmlToolMain skeleton

--- src/main/java/de/voomdoon/tool/map/kml/KmlToolMain.java
+++ src/main/java/de/voomdoon/tool/map/kml/KmlToolMain.java
@@ -1,7 +1,20 @@
 package de.voomdoon.tool.map.kml;
 
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since DOCME add inception version number
+ */
 public class KmlToolMain {
 
+	/**
+	 * DOCME add JavaDoc for method main
+	 * 
+	 * @param args
+	 * @since DOCME add inception version number
+	 */
 	public static void main(String[] args) {
 		// TODO implement main
 		throw new UnsupportedOperationException("Method 'main' not implemented yet");
--- src/test/java/de/voomdoon/tool/map/kml/KmlToolMainTest.java
+++ src/test/java/de/voomdoon/tool/map/kml/KmlToolMainTest.java
@@ -1,5 +1,12 @@
 package de.voomdoon.tool.map.kml;
 
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since DOCME add inception version number
+ */
 class KmlToolMainTest {
 
 }
